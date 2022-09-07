8c2bcd1d197dea815f8b4a424e082b40dcda1c1d
JingniHe <48270904+JingniHe@users.noreply.github.com>
2022-09-07 00:51:20 -0600

    Update WriteSQL.R

--- Simulations/Sim_TF/WriteSQL.R
+++ Simulations/Sim_TF/WriteSQL.R
@@ -8,8 +8,8 @@ args <- commandArgs(trailingOnly=T)
 trait<-args[1]
 curr_index<-args[2]
 
-working_dir="/work/long_lab/jingni/project/TF_TWAS/Simulation/Simulation_Revision/Sim_GWAS/Sim_TF/summary/"
+working_dir="./summary/"
 
-db <- dbConnect(SQLite(),dbname=working_dir%&%"GTEx_Pheno"%&%trait%&%"_"%&%curr_index%&%".db")
-dbWriteTable(conn=db,name="extra",value=working_dir%&%"GTEx_Pheno"%&%trait%&%"_"%&%curr_index%&%"_model_summaries_R0.01.txt",row.names=FALSE,header=TRUE)
-dbWriteTable(conn=db,name="weights",value=working_dir%&%"GTEx_Pheno"%&%trait%&%"_"%&%curr_index%&%"_weights.txt",row.names=FALSE,header=TRUE)
+db <- dbConnect(SQLite(),dbname=working_dir%&%"GTEx_"%&%curr_index%&%".db")
+dbWriteTable(conn=db,name="extra",value=working_dir%&%"GTEx_"%&%curr_index%&%"_model_summaries_R0.01.txt",row.names=FALSE,header=TRUE)
+dbWriteTable(conn=db,name="weights",value=working_dir%&%"GTEx_"%&%curr_index%&%"_weights.txt",row.names=FALSE,header=TRUE)
